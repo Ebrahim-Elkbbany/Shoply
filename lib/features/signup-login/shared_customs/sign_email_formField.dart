6fdeecb4bc251b37853d2cf434825b1ffd2dda0f
shimaa <shimaafoda@std.mans.edu.eg>
2024-02-03 23:50:37 +0200

    last commit

--- lib/features/signup-login/shared_customs/sign_email_formField.dart
+++ lib/features/signup-login/shared_customs/sign_email_formField.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class SignEmailField extends StatelessWidget {
   final TextEditingController controller;
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(controller.text);
+  }
 
   const SignEmailField({super.key, required this.controller});
   @override
@@ -13,14 +17,18 @@ class SignEmailField extends StatelessWidget {
         height: 70,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
+
           color: Colors.white,
         ),
         child: TextFormField(
           controller: controller,
+          keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(hintText: "Email"),
           validator: (value) {
             if (value == null || value.isEmpty) {
               return 'Please enter your password';
+            }else if (!isValidEmail) {
+              return 'Invalid email format';
             }
             return null;
           },
